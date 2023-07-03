686cdca92d9dc91a8a55404ba9863f6ba1af41e4
fatemanazari <fatemanazari78@gmail.com>
2023-07-03 14:56:12 +0430

    Add navbar, logo and mobile menu

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const body = document.querySelector("body");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        hamburger.classList.remove("active");
+    }))
