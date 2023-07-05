4bdce573d401e92cc0b4d3ba1ea2e577dc38f22c
fatemanazari <fatemanazari78@gmail.com>
2023-07-05 11:54:06 +0430

    Add featured designers dynamically with JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Mobile Menu
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
@@ -12,6 +13,79 @@ document.querySelectorAll('.nav-link').forEach((n) => n
     hamburger.classList.remove('active');
   }));
 
+//Featured Designers
+const designers = [
+  {
+    image: './images/designer1.jpg',
+    name: 'Ethan Marcotte',
+    role: 'Web Designer',
+    description: 'Ethan Marcotte is an independent designer/developer who is passionate about beautiful design, elegant code.'
+  },
+  {
+    image: './images/designer2.jpeg',
+    name: 'Sara Soueidan',
+    role: 'Web developer',
+    description: 'Sara Soueidan is an independent Web UI and design engineer, author, speaker, and trainer from Lebanon.'
+  },
+  {
+    image: './images/designer3.jpeg',
+    name: 'Cameron Moll',
+    role: 'Author',
+    description: 'Cameron Moll isn’t your average web designer. In fact, nothing Cameron.'
+  },
+  {
+    image: './images/designer4.jpeg',
+    name: 'Jeffrey Zeldman',
+    role: 'American entrepreneur',
+    description: 'Jeffrey Zeldman is a name that has become synonymous with the rise of the internet.'
+  },
+  {
+    image: './images/designer5.jpeg',
+    name: 'Dan Cederholm',
+    role: 'Author',
+    description: 'Designer, developer, banjoist, Dribbble co-founder, and all-around niceguy.'
+  },
+  {
+    image: './images/designer6.jpeg',
+    name: 'John Resig',
+    role: 'American software engineer',
+    description: ' John Resig is best known as an expert in the JavaScript programming language and the creator of the most popular JavaScript library in the world.'
+  }
+]
+
+function generateDesignersHTML() {
+  var designersContainer = document.getElementById('featured-designers');
+
+  var designersHTML = '';
+
+  for (var i = 0; i < designers.length; i++) {
+    var designer = designers[i];
+
+    designersHTML += `
+      <div class="featured-designers grid">
+          <img src="${designer.image}" alt="">
+          <div class="designers-details">
+              <h3>${designer.name}</h3>
+              <p class="role">
+                  ${designer.role}
+              </p>
+              <hr class="line">
+              <div class="designers-description">
+                  <p class="description">
+                    ${designer.description}
+                  </p>
+              </div>
+          </div>
+      </div>
+    `;
+  }
+
+  designersContainer.innerHTML = designersHTML;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  generateDesignersHTML();
+});
 
 
 
